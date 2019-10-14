e678e0b841c5adf8fa707b1a08a4469bed8a2cb2
laynebritton <laynebritton1998@gmail.com>
2019-10-14 00:05:28 -0400

    Races, traits, proficiencies

--- connectionTest.py
+++ connectionTest.py
@@ -30,6 +30,77 @@ def closeConnection(conn):
     conn.cursor().close()
 
 
+def insertTraitsFromJSON(conn):
+    with open('data/5e-SRD-Traits.json') as f:
+        traits_dict = json.load(f)
+
+    trait_insert = "INSERT INTO traits(tname,effect) VALUES(%s,%s)"
+    trait_race_insert = "INSERT INTO Traits_Provided_By_Race (tname,rname) VALUES (%s,%s)"
+
+    cur = conn.cursor()
+
+    for dnd_trait in traits_dict:
+
+        record_to_insert = (dnd_trait["name"], dnd_trait["desc"])
+
+        cur.execute(trait_insert, record_to_insert)
+        conn.commit()
+
+        for dnd_race in dnd_trait["races"]:
+            record_feature_class_insert = (dnd_trait["name"], dnd_race["name"])
+            cur.execute(trait_race_insert, record_feature_class_insert)
+            conn.commit()
+
+
+def insertRacesFromJSON(conn):
+    with open('data/5e-SRD-Races.json') as f:
+        races_dict = json.load(f)
+
+    race_insert = "INSERT INTO races(rname,speed) VALUES(%s,%s)"
+    proficiency_race_insert = "INSERT INTO Race_Provided_Proficiencies (rname,pname) VALUES (%s,%s)"
+
+    cur = conn.cursor()
+
+    for dnd_race in races_dict:
+
+        record_to_insert = (dnd_race["name"], dnd_race["speed"])
+
+        cur.execute(race_insert, record_to_insert)
+        conn.commit()
+
+        for dnd_subrace in dnd_race["subraces"]:
+            record_to_insert = (dnd_subrace["name"], dnd_race["speed"])
+            cur.execute(race_insert, record_to_insert)
+            conn.commit()
+
+        for dnd_proficiency in dnd_race["starting_proficiencies"]:
+            record_feature_class_insert = (dnd_race["name"], dnd_proficiency["name"])
+            cur.execute(proficiency_race_insert, record_feature_class_insert)
+            conn.commit()
+
+def insertProficienciesFromJSON(conn):
+    with open('data/5e-SRD-Proficiencies.json') as f:
+        features_dict = json.load(f)
+
+    proficiency_insert = "INSERT INTO proficiencies(pname,type) VALUES(%s,%s)"
+    proficiency_class_insert = "INSERT INTO Classes_Provided_Proficiencies (cname,pname) VALUES (%s,%s)"
+    # proficiency_race_insert = "INSERT INTO Classes_Provided_Proficiencies (cname,pname) VALUES (%s,%s)"
+
+    cur = conn.cursor()
+
+    for dnd_proficiency in features_dict:
+
+        record_to_insert = (dnd_proficiency["name"], dnd_proficiency["type"])
+
+        cur.execute(proficiency_insert, record_to_insert)
+        conn.commit()
+
+        for dnd_class in dnd_proficiency["classes"]:
+            record_feature_class_insert = (dnd_class["name"], dnd_proficiency["name"])
+            cur.execute(proficiency_class_insert, record_feature_class_insert)
+            conn.commit()
+
+
 def insertFeaturesFromJSON(conn):
     with open('data/5e-SRD-Features.json') as f:
         features_dict = json.load(f)
@@ -99,7 +170,9 @@ if __name__ == '__main__':
     conn = connect()
     # insertClassesFromJSON(conn)
     # insertSpellsFromJSON(conn)
-    insertFeaturesFromJSON(conn)
-    conn.cursor().close()
+    # insertFeaturesFromJSON(conn)
+    # insertProficienciesFromJSON(conn)
+    # insertRacesFromJSON(conn)
+    # insertTraitsFromJSON(conn)
     conn.close()
     print('done!')
